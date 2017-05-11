1f1a5b6a32bf87dc63163115510fe0f6b91d31f3
Vale Cofer-Shabica <vale@brown.edu>
2017-05-11 14:14:48 +0000

    chunks I forgot

--- src/mpist.c
+++ src/mpist.c
@@ -3,15 +3,18 @@
 #include <stdlib.h>
 #include <unistd.h> /*execv*/
 
-
-void help(char * pname){
-  printf("%s: An MPI wrapper to allow the execution of many instances of a\n
-         single-threaded program.\n", pname);
-}
+void help(char * pname);
 
 int main(int argc, char ** argv){
+  (void) argc;
   MPI_Init(NULL, NULL);
   help(argv[0]);
   MPI_Finalize();
-  return EXIT_SUCCESS
+  return EXIT_SUCCESS;
+}
+
+
+void help(char * pname){
+  printf("%s: An MPI wrapper to allow the execution of many instances of a\n"
+         "single-threaded program.\n", pname);
 }
