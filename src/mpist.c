830d6507b1c4398347d8a82ebbeed24c13c0d4ff
Vale Cofer-Shabica <vale@brown.edu>
2017-05-12 14:03:52 -0400

    comments!

--- src/mpist.c
+++ src/mpist.c
@@ -15,14 +15,17 @@ bool processOptions(int argc, char ** argv);
 #define BUFLEN 128
 
 struct param_t{
-  char * path;
-  char ** args;
-  char envvar[BUFLEN];
-  int n;
-  int m;
+  char * path;         // executable
+  char ** args;        // its arguments
+  char envvar[BUFLEN]; // environmental variable of interest
+  int n;               // low range
+  int m;               // high range
 };
 
-struct param_t param = {
+/*
+  Global struct holding parameters from the command line.
+*/
+static struct param_t param = {
   .path = NULL,
   .args = NULL,
   .envvar = {0},
@@ -43,17 +46,18 @@ int main(int argc, char ** argv){
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  {
+  { // Set the environmental var to rank + n
     char eval[BUFLEN];
     snprintf(eval, (size_t) BUFLEN, "%d", param.n + rank);
     setenv(param.envvar, eval, true);
   }
 
+  // if we're already past m, no need to run
   if (param.n + rank <= param.m){
     fprintf(stderr, "Launching from rank %d.\n", rank);
     int wstatus;
     pid_t pid = vfork();
-    if (0 == pid){
+    if (0 == pid){// Must make the execv immediately since using vfork.
       execv(param.path, param.args);
     }
     else{
@@ -70,7 +74,7 @@ void help(char * pname){
   printf("%s: An MPI wrapper to allow the execution of many instances of a\n"
          "  single-threaded program.\n", pname);
   printf("Usage: %s [-h] [-G] -C ENVVAR -t n-m EXE [ARGS]\n", pname);
-  printf("  Executes EXE [ARGS] with the environmental varaible specified by\n"
+  printf("  Executes EXE [ARGS] with the environmental variable specified by\n"
 	 "  ENVVAR set to integers in the range [n,m] inclusive. If there are\n"
 	 "  insufficient MPI ranks available, the top of the range is truncated.\n");
   printf("  -h displays this message.\n");
@@ -87,12 +91,15 @@ bool processOptions(int argc, char ** argv){
   bool go = true;
   
   int c;
-  opterr = 0;
+  opterr = 0;  //since we do our own error-handling
   const char optstring[]="+hGC:t:";
   while (-1 != (c = getopt(argc,argv, optstring))){
     switch (c){
     case 'C':
-      if (strlen(optarg) < BUFLEN){ // == case has insufficient space with '\0' char
+      /*
+	== case has insufficient space because of the NULL ('\0') char.
+      */
+      if (strlen(optarg) < BUFLEN){
 	strncpy(param.envvar, optarg, (size_t) BUFLEN);
       } else {
 	fprintf(stderr, "ENVVAR must be shorter than %d. Exiting!\n", BUFLEN - 1);
@@ -100,6 +107,10 @@ bool processOptions(int argc, char ** argv){
       }
       break;
     case 't':
+      /*
+	Short-circuit evaluation ensures n & m are both set prior to
+	comparison.
+      */
       if (2 != sscanf(optarg, "%d-%d", &param.n, &param.m) ||
 	  param.n > param.m){
 	fprintf(stderr, "Bad range specification. Exiting!\n");
@@ -123,11 +134,18 @@ bool processOptions(int argc, char ** argv){
     }
   }
 
+  /*
+    After the POSIXLY-CORRECT run of getopt, the argument at optind is
+    the executable and those that follow are its arguments.
+  */
   param.path = argv[optind];
   param.args = argv + optind + 1;
   return go;
 }
 
+/*
+  Displays the contents of param on stderr.
+*/
 void status(void){
   fprintf(stderr,"%s=[%d,%d] ", param.envvar, param.n, param.m);
   fprintf(stderr,"%s", param.path);
